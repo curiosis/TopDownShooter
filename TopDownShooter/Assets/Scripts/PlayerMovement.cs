80abd6c39cb9951d96fc700ffa8d2a6a19c3619f
RemigiuszDrobinski <curiosisciekawski@gmail.com>
2020-10-04 14:26:32 +0200

    Add player movement, shooting and bullet script with a few var.

--- TopDownShooter/Assets/Scripts/PlayerMovement.cs
+++ TopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    float angle;
+    public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 movement, mousePosition, lookDirection;
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        lookDirection = mousePosition - rb.position;
+
+        angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
